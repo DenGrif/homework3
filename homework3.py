8d63682e64c0e8ee6480ce0765e735138ab5ebfa
DenGrif <grifon991@gmail.com>
2024-06-03 22:42:34 +0400

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,10 @@
+name = 'Name: Denis'
+print(name)
+age = 'Age: 44'
+print(age)
+new_age = "New Age:"
+new_age1 = 44
+print(new_age, new_age1 + 1)
+is_student = 'Is Student:'
+is_student1 = True
+print(is_student, is_student1)
